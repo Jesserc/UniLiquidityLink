5fe5e5ac87764ea58d264cdd9fc6a26a50fc4155
Jesserc <raymondjesse713@gmail.com>
2022-12-29 11:44:07 +0100

    Jesserc: uniswap v2 add liquidity

--- contracts/UniswapLiquidity.sol
+++ contracts/UniswapLiquidity.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 import "./interface/IUniswapV2Factory.sol";
-import "./interface/IUniswapV2Router01.sol";
 import "./interface/IUniswapV2Router02.sol";
 import "./interface/IERC20.sol";
 
@@ -13,33 +12,71 @@ contract UniswapLiquidity {
     address UniswapV2Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    event Logs(string message, uint256 value);
+    event Logs(string message, uint value);
 
+    //add liquidity to uniswap pool
     function addLiquidity(
-        address tokenA,
-        address tokenB,
-        uint amountA,
-        uint amountB
-    )
-        external
-        returns (uint256 amountOne, uint256 amountTwo, uint256 liquidity)
-    {
-        IERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
-        IERC20(tokenB).transferFrom(msg.sender, address(this), amountB);
+        address _tokenA,
+        address _tokenB,
+        uint _amountA,
+        uint _amountB
+    ) external {
+        require(
+            IERC20(_tokenA).transferFrom(msg.sender, address(this), _amountA)
+        );
 
-        IERC20(tokenA).approve(UniswapV2Router, amountA);
-        IERC20(tokenB).approve(UniswapV2Router, amountB);
+        require(
+            IERC20(_tokenB).transferFrom(msg.sender, address(this), _amountB)
+        );
 
-        (amountOne, amountTwo, liquidity) = IUniswapV2Router02(UniswapV2Router)
-            .addLiquidity(
-                tokenA,
-                tokenB,
-                amountA,
-                amountB,
+        require(IERC20(_tokenA).approve(UniswapV2Router, _amountA));
+        require(IERC20(_tokenB).approve(UniswapV2Router, _amountB));
+
+        (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        ) = IUniswapV2Router02(UniswapV2Router).addLiquidity(
+                _tokenA,
+                _tokenB,
+                _amountA,
+                _amountB,
+                1,
+                1,
+                address(this),
+                block.timestamp + 20 seconds
+            );
+
+        emit Logs("amount A taken", amountA);
+        emit Logs("amount B taken", amountB);
+        emit Logs("Liquidity token received", liquidity);
+    }
+
+    //remove our liquidity added before
+    function removeLiquidity(
+        address _tokenA,
+        address _tokenB
+    ) external returns (address pair) {
+        //get pair address
+        pair = IUniswapV2Factory(UniswapV2Factory).getPair(_tokenA, _tokenB);
+        uint liquidity = IERC20(pair).balanceOf(address(this));
+
+        //approve router to remove our liquidity token
+        require(IERC20(pair).approve(UniswapV2Router, liquidity));
+
+        (uint amountA, uint amountB) = IUniswapV2Router02(UniswapV2Router)
+            .removeLiquidity(
+                _tokenA,
+                _tokenB,
+                liquidity,
                 1,
                 1,
                 address(this),
                 block.timestamp
             );
+
+        emit Logs("amount A gotten", amountA);
+        emit Logs("amount B gotten", amountB);
+        emit Logs("Liquidity token burnt", liquidity);
     }
 }
--- contracts/interface/IUniswapV2Router02.sol
+++ contracts/interface/IUniswapV2Router02.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.6.2;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 interface IUniswapV2Router02 {
     function factory() external pure returns (address);
